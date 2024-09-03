1b87b2dffa29cc934a3be639c572b9181830fa6d
WILLIAM TAVARES <112444968+MestreWil@users.noreply.github.com>
2024-09-02 21:15:09 -0300

    Update main.cs

--- aula2/main.cs
+++ aula2/main.cs
@@ -8,15 +8,28 @@ class Program
     public static void Main(string[] args)
     {
         Console.WriteLine("Criando Clientes: \n");
-        Cliente cliente1 = new Cliente("João", "11 99999-9999", "ztejd@example.com", "Rua A, 123");
-        Cliente cliente2 = new Cliente("Bruno", "11 99999-9999", "elchapo@example.com", "Rua B, 123");
+        //Cliente cliente1 = new Cliente("João", "11 99999-9999", "ztejd@example.com", "Rua A, 123");
+        //Cliente cliente2 = new Cliente("Bruno", "11 99999-9999", "elchapo@example.com", "Rua B, 123");
+
+        PF pf1 = new PF("MestreWill", "11 99999-9999", "111.111.111-11", "mestre@gmail.com", "Rua c,2121", PF.TipoEstadoCivil.Solteiro);
+
+        PJ pj1 = new PJ("Empresa X", "11 99999-9999", "Empresa@gmail.com", "Rua d,2121", "3592651981", new DateOnly(2022, 01, 01));
         Console.WriteLine("Criando Contas: \n");
 
-        Conta c1 = new Conta(123, cliente1);
+        Conta c1 = new Conta(123, pf1);
+        Conta c2 = new Conta(456, pj1);
 
         c1.Info();
         c1.Depositar(100);
         c1.Info();
         c1.Sacar(100);
+        c1.Depositar(300);
+        c1.Info();
+        c1.Transferir(100, c2);
+        c1.Info();
+        c2.Info();
+
+        pf1.Info();
+
     }
 }
