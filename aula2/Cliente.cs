fbbe6024e2d4fd3ce7b6b2dafa5cb32acc548e77
WILLIAM TAVARES <112444968+MestreWil@users.noreply.github.com>
2024-09-02 21:14:20 -0300

    Update Cliente.cs

--- aula2/Cliente.cs
+++ aula2/Cliente.cs
@@ -1,19 +1,23 @@
 using System;
 
-public class Cliente{
-  public string Nome {get; set;}
-  public string Telefone {get; set;}
-  public string Email {get; set;}
-  public string Endereco {get; set;}
+public abstract class Cliente
+{
+    public string Nome { get; set; }
+    public string Telefone { get; set; }
+    public string Email { get; set; }
+    public string Endereco { get; set; }
 
-  public Cliente(string nome, string telefone, string email, string endereco){
-    this.Nome = nome;
-    this.Telefone = telefone;
-    this.Email = email;
-    this.Endereco = endereco;
-  }
+    public Cliente(string nome, string telefone, string email, string endereco)
+    {
+        this.Nome = nome;
+        this.Telefone = telefone;
+        this.Email = email;
+        this.Endereco = endereco;
+    }
+    // A palavra reservada "virtual" indica que o metodo pode ser sobrescrito pelo seus herdeiros
 
-  public void Info(){
-    Console.Write("Nome: " + this.Nome + "\n" + this.Telefone + "\n" + this.Email + "\n" + this.Endereco);
-  }
+    public virtual void Info()
+    {
+        Console.Write("Nome: " + this.Nome + "\n" + this.Telefone + "\n" + this.Email + "\n" + this.Endereco + "\n");
+    }
 }
